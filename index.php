7fa6503897bd49e65260a234f3833ed4c2140bd2
Chun Ping Ng <chn22@pitt.edu>
2012-03-04 13:51:56 -0500

    grade_assig.php -> print out the source code

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	get_header();
 	include("includes/database.php");
 ?>
-
+This is the homepage.
 <br><a id='logout-anchor' style="margin: 20px;" href='javascript:logout()'>Logout</a><br><br>
 
 <? get_footer(); ?>
