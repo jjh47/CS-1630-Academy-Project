87e2871116a895088e29bb96654b3c378714477f
James Hoegerl <jjh47@pitt.edu>
2012-03-13 17:28:12 -0400

    More features added to process_login.php

--- includes/defines.php
+++ includes/defines.php
@@ -9,6 +9,7 @@ define("DB_NAME","cs1630Academy");
 define("DB_PATH","academyprojectdb"); //path to the database file
 define("HOME_DIR","http://localhost:8888/");
 define("CLASS_PATH","/Users/jjh47/Documents/CS-1630-Academy-Project/");
+define("GSCRIPT_PATH", "/Users/jjh47/Documents/CS-1630-Academy-Project/pages/grade_assig.php");
 
 define("MODE","dev");
 	
