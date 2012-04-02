afbcc726e6b69e129803f2d4f723b6fee3b742e4
Rafe Colton <rafael.colton@gmail.com>
2012-04-02 18:42:08 -0400

    Added instructuions to add_user by csv

--- pages/add_user.php
+++ pages/add_user.php
@@ -50,6 +50,8 @@
 		<input type="submit" value="Upload File" name="uploadcsv" />
 		<? add_token(); ?>
 	</form>
+	<em><div style='font-size: 12px; margin-top: 15px;'>File must be formated as (username, email, user type, password).
+		If you <br>are exporting data from Excel, please make sure to remove the column headers.</div></em>
 	<br>
 
 <script>
