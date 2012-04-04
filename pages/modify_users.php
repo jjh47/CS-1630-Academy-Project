7bcb9e8e2671c5ba1b72051f9e21f0b7dbbead0e
Brian Sisco <bartolo42@gmail.com>
2012-04-04 02:38:12 -0400

    client side code for modify_users

--- pages/modify_users.php
+++ pages/modify_users.php
@@ -32,6 +32,7 @@
 	else{ //Now we pull the data for the table from the database.
 		//This should be enough to initially populate the table.
 		$results = $db->arrayQuery("select user_id, username, email, usertype from User");
+		$classes = $db->arrayQuery("select class_id, class_name from Class");
 		//Change needed:
 		//May need another request to verify changes, such as having the enrollment table to approve/deny enrollment changes
 		
@@ -42,35 +43,115 @@
 <!-- Now we have the HTML for displaying the table. After this works, add on DataTables -->
 <h1>Modify Users</h1>
 <!-- TODO: create page process_modify_users.php (or do it another way, inline?) and function (below) submit_modify_users -->
-<form id="modify_users" method="post" action="process_modify_users.php" onsubmit="return submit_modify_users()">
-<table id="users_table">
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Type</th>
-            <th>Password</th>
-			<th>Select</th>
-        </tr>
-    </thead>
-    <tbody>
-	<!-- It is now time to go to php to populate the table with info from $results -->
-	<?php
-	//$arrayLength = count($results); //Delete if not used at end
-	//for($i = 0; $i <= $arrayLength; $i = $i + 6)
-	foreach ($results as $entry)
-	{ 
-      echo "<tr>"; 
-      echo "<td>{$entry['username']}</td><td>{$entry['usertype']}</td><td>text box goes here</td><td>checkbox goes here</td>"; 
-      echo "</tr>";     
-	} 
-	?>
-	
-    
-	
-    </tbody>
-</table>
+<!-- onsubmit="return submit_modify_users()" -->
+<form id="modify_users" method="post" action="process_modify_users.php" >
+	<select name="class_name" id="class_name" style='width: 130px;'>
+		<option value="">Classes...</option>
+		<? 
+		for($i = 0; $i < count($classes); $i++){
+			echo "<option value='". $classes[$i]['class_name']."'>". $classes[$i]['class_name']. "</option>";
+		}
+		?>
+	</select>
+	<input type="submit" name="modifySubmit" onclick="return clickEnroll()" value="Enroll"/>&nbsp;
+	<input type="submit" name="modifySubmit" onclick="return clickDelete()" value="Delete Users"/>&nbsp;
+	<input type="submit" name="modifySubmit" onclick="return clickPassword()" value="Change Passwords"/>&nbsp;
+	<input type="reset" value="Reset">
+	<? add_token(); ?>
+
+	<table id="users_table">
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Type</th>
+				<th>Password</th>
+				<th>Select</th>
+			</tr>
+		</thead>
+		<tbody>
+		<!-- It is now time to go to php to populate the table with info from $results -->
+		<?php
+		//$arrayLength = count($results); //Delete if not used at end
+		//for($i = 0; $i <= $arrayLength; $i = $i + 6)
+		foreach ($results as $entry)
+		{ 
+		  //It's easier to echo this out if it is stored this way.
+		  $id = $entry['user_id'];
+		  echo "<tr>";
+		  //First we have the particular user's name and usertype.
+		  echo "<td>{$entry['username']}</td><td>{$entry['usertype']}</td>";
+		  //Now we get a text input box for inputing a new password. 
+		  echo "<td><input type='text' name='password_$id' id='password_$id' style='width: 100px;'/></td>";
+		  //Now a checkbox for applying the given action to this person.
+		  echo "<td><input type='checkbox' name='check_$id' id='check_$id' onclick='checkClick($id)' /></td>"; 
+		  echo "</tr>";     
+		} 
+		?>
+		
+		
+		
+		</tbody>
+	</table>
 </form>
+	
 
+<script type="text/javascript">
+	var checkedCount = 0;
+	
+	//Keeps track of how many checkboxes are checked, so that we can refuse to submit a page with none checked.
+	function checkClick(id_num)
+	{
+		var id = "check_".concat(id_num);
+		var checkbox = document.getElementById(id);
+		if(checkbox.checked == true)
+		{
+			checkedCount++;
+		}
+		else
+		{
+			checkedCount--;
+		}
+	}
+	
+	//If the Enroll button is hit, make sure a class is selected and at least one user is selected.
+	function clickEnroll()
+	{
+		if($('#class_name').val() == "" || $('#class_name').val() == null){
+			alert("Please select a class in which to enroll the students.");
+			return false;
+		}
+		if(checkedCount <= 0)
+		{
+			alert("Please select a student to enroll.")
+			return false;
+		}
+		return true;
+	}
+	
+	//If the Delete button is hit, there should be a user selected and a confirmation box should pop up.
+	function clickDelete()
+	{
+		if(checkedCount <= 0)
+		{
+			alert("Please select a user before deleting.")
+			return false;
+		}
+		else
+		{
+			var ok = confirm("Select ok to confirm deletion");
+			return ok;
+		}
+		return true;
+	}
+	
+	//If the Change Password button is hit, none of the relevant password boxes should be blank.
+	function clickPassword()
+	{
+		//I'll get back to this.
+		return true;
+	}
+	
 
+</script>
 
 <? get_footer(); ?>
